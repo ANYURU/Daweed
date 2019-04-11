73b3fc16173aa3fb077dd9c75ab057c09b1f9aac
anyuru <davidderrickanyuru@gmail.com>
2019-04-11 14:23:53 -0700

    work.java

---
+++ work.java
@@ -0,0 +1,9 @@
+public class Work{
+	public static void main(String[] argv){
+	int a = 4;
+	int b = 2;
+	int c = a + b;
+	System.out.println("This is my first program");
+	System.out.printf("%d",c);
+}
+}
